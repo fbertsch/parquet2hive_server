fb19698778f40bced994cad9995e7ea25422de67
Frank Bertsch <fbertsch@mozilla.com>
2017-03-27 13:01:30 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='parquet2hive_server',
-      version='0.0.3',
+      version='0.0.4',
       author='Frank Bertsch',
       author_email='fbertsch@mozilla.com',
       description='API Endpoint for Parquet2hive',
